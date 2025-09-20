f9bc0896f4abc19138c95758121af3a3c5d6a893
yuval-po <yuval.pomer@protonmail.com>
2025-09-20 19:35:10 +0300

    Added an 'About' section and moved entries

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title} - Go API made easy`}
       description="Gleece - A Golang tool for building, documenting, validating securing and customizing APIs through code-first development.">
       <HomepageHeader />
       <main>
